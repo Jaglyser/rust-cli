2fa546bf7a67894766c3445605b7ad7353bbcddf
Jaglyser <joelwblomster@gmail.com>
2022-01-13 22:50:41 +0100

    file search

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    let query = &args[0];
+    let filename = &args[1];
+    println!("searching for {}", query);
+    println!("in file {}", filename);
 }
